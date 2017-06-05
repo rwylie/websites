27fd34d2428f0f5a974073e6442c7a6ddf3937f5
Ronda Wylie <rondawylie@yahoo.com>
2017-06-05 15:15:56 -0500

    portfolio2

---
+++ portfolio2/main.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $(".link1").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+      // Store hash
+      var hash = this.hash;
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+  $(".link2").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+  $(".link3").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
+
+// var initTopPosition= $('.thirda, .thirdb').offset().top;
+// $(window).scroll(function(){
+//     if($(window).scrollTop() > initTopPosition)
+//         $('.thirda, .thirdb').css({'position':'fixed','top':'0px'});
+//     else
+//         $('.thirda, .thirdb').css({'position':'absolute','top':initTopPosition+'px'});
+// });
